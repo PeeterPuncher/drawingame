3310426bede1af9e8b25761a949005356d96f929
PeeterPuncher <samai.mc.adam@gmail.com>
2025-02-01 16:10:09 +0100

    f

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ function updateLobby() {
   const roomList = Object.entries(rooms).map(([roomId, room]) => ({
     roomId,
     name: room.name,
-    players: room.clients.size
+    players: room.clients.size,
   }));
+
+  console.log('Broadcasting lobby update:', roomList); // Log the room list being sent
 
   wss.clients.forEach((client) => {
     if (client.readyState === WebSocket.OPEN) {
@@ -29,18 +31,28 @@ function updateLobby() {
   });
 }
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws) =>
+{
   console.log('New client connected');
 
-  ws.on('message', (message) => {
+  ws.on('message', (message) =>
+  {
     const data = JSON.parse(message);
     console.log('Message received from client:', data);
 
-    if (data.type === 'create-room') {
+    if (data.type === 'get-lobby')
+    {
+      updateLobby(); // Send the latest room list to the client
+    }
+
+    else if (data.type === 'create-room')
+    {
       let roomId;
-      do {
+
+      while (rooms[roomId])
+      {
         roomId = generateRoomCode();
-      } while (rooms[roomId]);
+      }
 
       rooms[roomId] = { name: data.roomName || `Room ${roomId}`, clients: new Set() };
       ws.roomId = roomId;
@@ -49,22 +61,35 @@ wss.on('connection', (ws) => {
       console.log(`Room created: ${roomId}`);
       ws.send(JSON.stringify({ type: 'room-created', roomId }));
       updateLobby();
-    } else if (data.type === 'join-room') {
+    }
+
+    else if (data.type === 'join-room')
+    {
       const { roomId } = data;
-      if (rooms[roomId]) {
+
+      if (rooms[roomId])
+      {
         ws.roomId = roomId;
         rooms[roomId].clients.add(ws);
         console.log(`Client joined room: ${roomId}`);
         ws.send(JSON.stringify({ type: 'room-joined', roomId }));
         updateLobby();
-      } else {
+      }
+      else
+      {
         ws.send(JSON.stringify({ type: 'error', message: 'Room not found' }));
       }
-    } else if (data.type === 'message') {
+    }
+    else if (data.type === 'message')
+    {
       const { roomId, text, user } = data;
-      if (rooms[roomId]) {
-        rooms[roomId].clients.forEach((client) => {
-          if (client.readyState === WebSocket.OPEN) {
+
+      if (rooms[roomId])
+      {
+        rooms[roomId].clients.forEach((client) =>
+        {
+          if (client.readyState === WebSocket.OPEN)
+          {
             client.send(JSON.stringify({ type: 'message', user, text }));
           }
         });
@@ -73,11 +98,15 @@ wss.on('connection', (ws) => {
     }
   });
 
-  ws.on('close', () => {
+  ws.on('close', () =>
+  {
     console.log('Client disconnected');
-    if (ws.roomId && rooms[ws.roomId]) {
+
+    if (ws.roomId && rooms[ws.roomId])
+    {
       rooms[ws.roomId].clients.delete(ws);
-      if (rooms[ws.roomId].clients.size === 0) {
+      if (rooms[ws.roomId].clients.size === 0)
+      {
         delete rooms[ws.roomId];
         console.log(`Room deleted: ${ws.roomId}`);
       }
