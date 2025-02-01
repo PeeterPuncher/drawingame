a78ea9323362650acbb11f626aa3efb84ae3f502
PeeterPuncher <samai.mc.adam@gmail.com>
2025-02-01 15:52:12 +0100

    é

--- server.js
+++ server.js
@@ -30,8 +30,11 @@ function updateLobby() {
 }
 
 wss.on('connection', (ws) => {
+  console.log('New client connected');
+
   ws.on('message', (message) => {
     const data = JSON.parse(message);
+    console.log('Message received from client:', data);
 
     if (data.type === 'create-room') {
       let roomId;
@@ -43,23 +46,21 @@ wss.on('connection', (ws) => {
       ws.roomId = roomId;
       rooms[roomId].clients.add(ws);
 
+      console.log(`Room created: ${roomId}`);
       ws.send(JSON.stringify({ type: 'room-created', roomId }));
       updateLobby();
-    } 
-    
-    else if (data.type === 'join-room') {
+    } else if (data.type === 'join-room') {
       const { roomId } = data;
       if (rooms[roomId]) {
         ws.roomId = roomId;
         rooms[roomId].clients.add(ws);
+        console.log(`Client joined room: ${roomId}`);
         ws.send(JSON.stringify({ type: 'room-joined', roomId }));
         updateLobby();
       } else {
         ws.send(JSON.stringify({ type: 'error', message: 'Room not found' }));
       }
-    } 
-    
-    else if (data.type === 'message') {
+    } else if (data.type === 'message') {
       const { roomId, text, user } = data;
       if (rooms[roomId]) {
         rooms[roomId].clients.forEach((client) => {
@@ -67,20 +68,24 @@ wss.on('connection', (ws) => {
             client.send(JSON.stringify({ type: 'message', user, text }));
           }
         });
+        console.log(`Message broadcasted in room: ${roomId}`);
       }
     }
   });
 
   ws.on('close', () => {
+    console.log('Client disconnected');
     if (ws.roomId && rooms[ws.roomId]) {
       rooms[ws.roomId].clients.delete(ws);
       if (rooms[ws.roomId].clients.size === 0) {
         delete rooms[ws.roomId];
+        console.log(`Room deleted: ${ws.roomId}`);
       }
       updateLobby();
     }
   });
 });
+
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/room', (req, res) => res.sendFile(path.join(__dirname, 'public', 'room.html')));
