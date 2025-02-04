b6dd20229601c9035782980636e661e0bc2ee010
PeeterPuncher <samai.mc.adam@gmail.com>
2025-02-04 15:39:40 +0100

    Update server.php

--- server.php
+++ server.php
@@ -1,7 +1,17 @@
 <?php
 require 'database.php';
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
+// Handle preflight (OPTIONS) requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
+{
+    http_response_code(200);
+    exit();
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
