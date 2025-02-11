35bf7e0d76f7df9887d966e98caefc0357663139
PeeterPuncher <samai.mc.adam@gmail.com>
2025-02-11 15:03:52 +0100

    é

--- server.js
+++ server.js
@@ -52,17 +52,14 @@ wss.on('connection', (ws) => {
           ws.send(JSON.stringify({ type: 'room-created', data: { ...responseData, roomCode, username } }));
 
           fetchData('join-room', { room_code: roomCode, user_name: username })
-          .then((responseData) => {
-            return new Promise((resolve) => {
-              setTimeout(() => {
-                rooms.get(roomCode).add(ws); // Add the client to the room
-                ws.roomCode = roomCode; // Set the room code
-                ws.username = username; // Set the username
-                ws.hasJoinedRoom = true; // Mark the user as having joined the room
-                ws.send(JSON.stringify({ type: 'room-joined', data: responseData }));
-                resolve();
-              }, 2000); // Wait for 1 second
-            });
+          .then((responseData) =>
+          {
+            waitFor(1000); // Wait for 1 second
+            rooms.get(roomCode).add(ws); // Add the client to the room
+            ws.roomCode = roomCode; // Set the room code
+            ws.username = username; // Set the username
+            ws.hasJoinedRoom = true; // Mark the user as having joined the room
+            ws.send(JSON.stringify({ type: 'room-joined', data: responseData }));
           })
           .catch((error) => {
             console.error('Fetch error:', error);
@@ -85,18 +82,13 @@ wss.on('connection', (ws) => {
       }
     
       fetchData('join-room', { room_code: roomCode, user_name: username })
-      .then((responseData) => {
-        return new Promise((resolve) => {
-          setTimeout(() => {
-            rooms.get(roomCode).add(ws); // Add the client to the room
-            ws.roomCode = roomCode; // Set the room code
-            ws.username = username; // Set the username
-            ws.hasJoinedRoom = true; // Mark the user as having joined the room
-            ws.send(JSON.stringify({ type: 'room-joined', data: responseData }));
-            resolve();
-          }, 2000); // Wait for 1 second
-        });
-      })
+        .then((responseData) => {
+          rooms.get(roomCode).add(ws); // Add the client to the room
+          ws.roomCode = roomCode; // Set the room code
+          ws.username = username; // Set the username
+          ws.hasJoinedRoom = true; // Mark the user as having joined the room
+          ws.send(JSON.stringify({ type: 'room-joined', data: responseData }));
+        })
         .catch((error) => {
           console.error('Fetch error:', error);
           ws.send(JSON.stringify({ type: 'error', message: error.message }));
