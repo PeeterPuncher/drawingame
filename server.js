ee7a9ff5f07767f3ea88b119cd46e4c04e191340
PeeterPuncher <samai.mc.adam@gmail.com>
2025-02-25 14:25:42 +0100

    fasz

--- server.js
+++ server.js
@@ -221,6 +221,7 @@ wss.on('connection', (ws) => {
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/room', (req, res) => res.sendFile(path.join(__dirname, 'public', 'room.html')));
+app.get('/canvas', (req, res) => res.sendFile(path.join(__dirname, 'public', 'room.html')));
 
 server.listen(3000, () => console.log('Server running on http://localhost:3000'));
 
