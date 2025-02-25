99449eb2faa34225c54b6390cd12c39482a59f2c
prokaiaron <prokagaming@gmail.com>
2025-02-25 14:19:12 +0100

    .

---
+++ RAJZOLÁS/upload.php
@@ -0,0 +1,21 @@
+<?php
+// A fájl nevét itt beállíthatjuk, vagy dinamikusan generálhatjuk
+$uploadDir = 'uploads/';
+$fileName = 'canvas_image_' . time() . '.png';
+
+if (isset($_POST['image'])) {
+    // A kép base64 kódolt adatainak dekódolása
+    $imageData = $_POST['image'];
+
+    // Levágjuk az adat URI prefixet (data:image/png;base64,)
+    $imageData = str_replace('data:image/png;base64,', '', $imageData);
+    $imageData = base64_decode($imageData);
+
+    // A fájl mentése
+    file_put_contents($uploadDir . $fileName, $imageData);
+
+    echo 'A kép sikeresen feltöltve!';
+} else {
+    echo 'Nincs kép adat.';
+}
+?>
