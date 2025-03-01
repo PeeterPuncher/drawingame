66222455150f4620944d078d58026b18352201bb
prokaiaron <prokagaming@gmail.com>
2025-03-01 17:44:01 +0100

    Update server.php

--- server.php
+++ server.php
@@ -120,15 +120,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 else if ($action == "save-drawing")
 {
-        $room_code = $data["room_code"];
+        // A fájl nevét itt beállíthatjuk, vagy dinamikusan generálhatjuk
+        $uploadDir = 'drawings/';
+        $fileName = 'canvas_image_' . time() . '.png';
 
-        if ($room_code != "")
-        {
-                $sql = "DELETE FROM rooms WHERE code = '$room_code'";
-                if (mysqli_query($conn,$sql))
-                {
-                        echo json_encode(["status"=> "success","data"=> "Room deleted"]);
-                }
+        if (isset($_POST['image'])) {
+        // A kép base64 kódolt adatainak dekódolása
+        $imageData = $_POST['image'];
+
+        // Levágjuk az adat URI prefixet (data:image/png;base64,)
+        $imageData = str_replace('data:image/png;base64,', '', $imageData);
+        $imageData = base64_decode($imageData);
+
+        // A fájl mentése
+        file_put_contents($uploadDir . $fileName, $imageData);
+
+        echo 'A kép sikeresen feltöltve!';
+        } else {
+        echo 'Nincs kép adat.';
         }
 }
 
