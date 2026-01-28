527e2c4e3d76a39a7bfe3231cecda2a980c5eeb0
PeeterPuncher <samai.mc.adam@gmail.com>
2026-01-28 12:37:39 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,13 @@ const baseUrl = 'https://gamedb.alwaysdata.net';
 const port = 3000;
 
 server.listen(port, () => {
-  console.log(`Server running on 3 redbulls and prayers, please be patient...`);
+  console.log(`"\x1b[32m [+] \x1b[0m" authenticating...`);
+  setTimeout(() => {
+    console.log(`"\x1b[32m [+] \x1b[0m" mapping the driver...`);
+  }, 500);
+  setTimeout(() => {
+    console.log(`"\x1b[31m [-] \x1b[0m" fuck you nigga`);
+  }, 1000);
 });
 
 app.use(express.static('public'));
